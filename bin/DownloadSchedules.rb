a283afe1b3f94ae2d5f432143495d15fc897d524
Straker Carryer (strakerc) <straker@carryer.com>
2018-02-10 22:20:23 -0500

    Add gems and create mailer

--- bin/DownloadSchedules.rb
+++ bin/DownloadSchedules.rb
@@ -5,8 +5,12 @@ require 'pp'
 doc = Nokogiri::HTML(open("https://www.majorityleader.gov/weekly-schedule/"))
 
 title = doc.title
-schedule = doc.search('p').map(&:text)
+#schedule = doc.search('p').map(&:text)
+schedule = doc.search('p')
+links = doc.css('p strong a')
+hrefs = links.map {|link| link.attribute('href').to_s}
 
 # Print data
 puts title
 pp schedule
+pp hrefs
