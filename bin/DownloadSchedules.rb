7fc35ceb317211be71f10a5ceda494ab15a6bb21
Straker Carryer (strakerc) <straker@carryer.com>
2018-02-11 13:46:52 -0500

    Sort and organize data from house leader weekly calendar, example mailer

--- bin/DownloadSchedules.rb
+++ bin/DownloadSchedules.rb
@@ -2,15 +2,37 @@ require 'open-uri'
 require 'nokogiri'
 require 'pp'
 
-doc = Nokogiri::HTML(open("https://www.majorityleader.gov/weekly-schedule/"))
+# Initialize structs
+@link_map = Hash.new
+@days_map = Hash.new
+@doc = Nokogiri::HTML(open("https://www.majorityleader.gov/weekly-schedule/"))
 
-title = doc.title
-#schedule = doc.search('p').map(&:text)
-schedule = doc.search('p')
-links = doc.css('p strong a')
-hrefs = links.map {|link| link.attribute('href').to_s}
+def main
+  daily_desc = @doc.css('p')
+  links = @doc.css('p strong a')
+  daily_desc.each do |desc|
+    day = desc.css('u').text
+    if day.blank? 
+      next
+    end
+    @days_map[day] = filtered(desc.text)
+  end
 
-# Print data
-puts title
-pp schedule
-pp hrefs
+  links.each do |link|
+    @link_map[link.text] = link.attribute('href').to_s
+  end
+end
+
+def print
+  p @doc.title
+  pp @link_map
+  pp @days_map
+end
+
+def filtered (text)
+  text.slice!(/.+?[0-9](TH|RD|ST|ND)/)
+  return text
+end
+
+main
+print
