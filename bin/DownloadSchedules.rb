ffc46f608ef7ea12e8bbb2e46c733dbe0ca62af9
Straker Carryer (strakerc) <straker@carryer.com>
2018-02-14 15:07:45 -0500

    Add bills to hash for each day

--- bin/DownloadSchedules.rb
+++ bin/DownloadSchedules.rb
@@ -4,41 +4,50 @@ require 'pp'
 
 # Initialize structs
 @link_descs = Hash.new{|hsh,key| hsh[key] = {} }
-@days_map = Hash.new
-@bill_descs = Array.new
+@days_map = Hash.new{|hsh,key| hsh[key] = {} }
+#@days_map = Hash.new
+@bill_text = Array.new
+@links_agg = Array.new
 @doc = Nokogiri::HTML(open("https://www.majorityleader.gov/weekly-schedule/"))
 
 def main
   daily_desc = @doc.css('p')
-  links = @doc.css('p strong a')
+  curr_day = ""
+  curr_day_desc = ""
 
-  # Get each day and summary
+  # Get all links, descriptions, and days
   daily_desc.each do |desc|
-    day = desc.css('u').text
-    sponsor = desc.css('em').text
-    all_text = desc.children.find {|n| n.text? && n.to_s.include?("–")}.to_s
-    all_text << sponsor unless sponsor.blank?
-    if day.blank? && all_text.blank? 
-      next
-    elsif day.blank? && !all_text.blank?
-      @bill_descs << all_text
-    elsif !day.blank? && all_text.blank?
-      @days_map[day] = date_filtered(desc.text)
-    else
-      @days_map[day] = date_filtered(desc.text)
-      @bill_descs << all_text
-    end
-  end
 
-  links.each do |link|
-    @link_descs[link.text] = link.attribute('href').to_s, @bill_descs.shift
+    # Get bill descriptions
+    sponsor = desc.css('em').text
+    bill_desc = desc.children.find {|n| n.text? && n.to_s.include?("–")}.to_s
+    bill_desc << sponsor unless sponsor.blank?
+    @bill_text << bill_desc unless bill_desc.blank?
+    
+    # Get link
+    link = desc.css('strong a')
+    if !link.blank?
+      bill = link.text
+      @links_agg << bill
+      @link_descs[bill] = link.attribute('href').to_s, @bill_text.shift
+      @days_map[curr_day] = curr_day_desc, @links_agg.clone
+    end
+    
+    # Get day
+    day = desc.css('u').text
+    if !day.blank?
+      curr_day = day
+      curr_day_desc = date_filtered(desc.text)
+      @days_map[curr_day] = curr_day_desc
+      @links_agg.clear
+    end
   end
 end
 
 def print
   p @doc.title
-  pp @link_descs
   pp @days_map
+  pp @link_descs
 end
 
 def date_filtered (text)
